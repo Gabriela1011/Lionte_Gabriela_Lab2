c959f0e87bdcfc628dd0bc30cafcf466e481a2cb
GABRIELA-MIHAELA LIONTE <gabriela.lionte@stud.ubbcluj.ro>
2024-11-13 15:10:14 +0200

    Laborator3

--- Models/Book.cs
+++ Models/Book.cs
@@ -23,5 +23,7 @@ namespace Lionte_Gabriela_Lab2.Models
 
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
